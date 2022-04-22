5a3f26091ad9ca173007bdf247e3e87680bec835
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-04-21 21:36:06 -0500

    adding delete button

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,22 +12,54 @@ struct ContentView: View {
    @StateObject var keychain: Keychain = Keychain()
    
     var body: some View {
+       
+       
        VStack{
           if keychain.haveUser{
-             if let use = keychain.user{
+             if let use = keychain.user {
                 Text("\(use)")
              }
+             HStack{
+             HStack(){
+                Button(action: { }) {
+                   HStack {Text("Delete User")}.padding(10.0)
+                      .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
+                }.padding(.trailing,10)
+             }
+             Spacer()
+             HStack(){
+                Button(action: { }) {
+                   HStack {Text("Delete All")}.padding(10.0)
+                      .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
+                }.padding(.trailing,10)
+             }
+             }.padding()
+          }else{
+             // MARK: Inserstion Sort
+             HStack(alignment: .center, spacing: 8, content: {
+                HStack(){
+                   Button(action: { }) {
+                      HStack {Text("Create User")}.padding(10.0)
+                         .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2.0))
+                   }.padding(.leading)
+                }
+                //Spacer()
+                
+             })
           }
+          
        }.environmentObject(keychain)
        .onAppear{
           Task {
               //When `purchasedIdentifiers` changes, get the latest subscription status.
-             
              try await keychain.exists(account: "username")
              try await keychain.get(account: "username")
           }
           
        }
+       .hideNavigationBar()
+       .edgesIgnoringSafeArea(.all)
+       .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -35,4 +67,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension View {
+   func hideNavigationBar() -> some View {
+      modifier(HideNavigationBarModifier())
+   }
+}
+struct HideNavigationBarModifier: ViewModifier {
+   func body(content: Content) -> some View {
+      content
+         .navigationBarBackButtonHidden(true)
+         .navigationBarHidden(true)
+         .navigationBarTitle("")
+   }
 }
